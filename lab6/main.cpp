cc13024e9322c92a655723a82f98732b9ec6bac7
zhabaa <vova.dovnar2006@gmail.com>
2024-10-19 02:44:57 +0200

    fix 6th lab

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <random>
-#include <ctime>
-#include <limits>
-#include <unordered_set>
+#include <cmath>
+#include <limits.h>
 
 using namespace std;
 
-int sumOfDigits(int digit) {
-    int sum = 0;
+const int MAX_N = 10'000;
 
-    while (digit > 0) {
-        sum += digit % 10;
-        digit /= 10;
+void printSequence(int numbers[MAX_N], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << numbers[i] << " ";
     }
+}
 
+int sumOfDigits(int x) {
+    int sum = 0;
+    while (x > 0) {
+        sum += x % 10;
+        x /= 10;
+    }
     return sum;
 }
 
-int getFirstDigit(int num) {
-    while (num >= 10) {
-        num /= 10;
+int getFirstDigit(int x) {
+    while (x >= 10) {
+        x /= 10;
     }
-    return num;
+    return x;
 }
 
-int getMaxDigit(int num) {
-    int maxDigit = 0;
-    while (num > 0) {
-        int digit = num % 10;
-        if (digit > maxDigit) {
-            maxDigit = digit;
-        }
-        num /= 10;
+int getMaxDigit(int x) {
+    int max = -1;
+    while (x > 0) {
+        max = fmax(max, x % 10);
+        x /= 10;
     }
-    return maxDigit;
+    return max;
 }
 
 bool customComparator(int a, int b) {
@@ -56,198 +56,195 @@ bool customComparator(int a, int b) {
     return a < b;
 }
 
-void replaceNegativeElements(vector<vector<int>>& matrix, int column) {
-    for (int i = 0; i < matrix.size(); ++i) {
-        if (matrix[i][column] < 0) {
-            matrix[i][column] = 0;
+bool hasDuplicates(int x) {
+    int digits[10] = {0};
+
+    while (x > 0) {
+        int digit = x % 10;
+        if (digits[digit] > 0) {
+            return true; 
         }
+        digits[digit]++;
+        x /= 10;
     }
+    return false;
 }
 
-void printMatrix(const vector<vector<int>>& matrix) {
-    for (const auto& row : matrix) {
-        for (int elem : row) {
-            cout << elem << "\t";
-        }
-        cout << endl;
-    }
-}
-
-bool duplicateDigits(int number) {
-    unordered_set<int> digits;
-    while (number > 0) {
-        int digit = number % 10;
-        if (digits.find(digit) != digits.end()) {
+bool hasFive(int x) {
+    int count = 0;
+    while (x > 0) {
+        if (x % 10 == 5) {
             return true;
         }
-        digits.insert(digit);
-        number /= 10;
+        x /= 10;
     }
     return false;
 }
 
 int first() {
-    srand(time(nullptr));
-
+    int numbers[MAX_N];
     int n;
-    cout << "Введите количество элементов последовательности (n <= 10000): ";
+
+    cout << "Введите количество элеметнов <= 10000: ";
     cin >> n;
 
-    if (n > 10000) {
-        cerr << "Количество элементов должно быть не более 10000.\n";
-        return 1;
+    cout << "Введите последовательность: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> numbers[i];
     }
 
-    vector<int> numbers(n);
-    
-    for (int& num : numbers) {
-        num = rand();
-    }
-
-    cout << "Последовательность чисел: ";
-    for (const int& num : numbers) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    bool flag = false;
+    bool hasSum14 = false;
 
     for (int i = 0; i < n; i++) {
         if (sumOfDigits(numbers[i]) == 14) {
-            flag = true;
+            hasSum14 = true;
             break;
         }
     }
 
-    if (flag) {
-        sort(numbers.begin(), numbers.end());
-        cout << "Отсортированная последовательность чисел: ";
-
-        for (const int& num : numbers) {
-            cout << num << " ";
-        }
-    } else {
-        cout << "В последовательности нет элементов, сумма которых равна 14" << endl;
+    if (!hasSum14) {
+        cout << "Нет чисел, сумма цифр которых равна 14." << endl;
+        printSequence(numbers, n);
+        return 0;
     }
+
+    sort(numbers, numbers + n);
+
+    cout << "Отсортированная последовательность: ";
+    printSequence(numbers, n);
 
     return 0;
 }
 
 int second() {
     int n;
-    cout << "Введите количество элементов (n <= 1000): ";
+
+    cout << "Введите количество элеметнов <= 10000: ";
     cin >> n;
 
-    if (n > 1000) {
-        cerr << "Количество должно быть не более 1000.\n";
-        return 1;
+    int numbers[n];
+    
+    cout << "Введите последовательность: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> numbers[i];
     }
 
-    vector<int> numbers(n);
-
-    cout << "Введите элементы: " << endl;
-    for (int &num : numbers) {
-        cin >> num;
-    }
-
-    sort(numbers.begin(), numbers.end(), customComparator);
-
-    cout << "Отсортированная последовательность: ";
-    for (const int &num : numbers) {
-        cout << num << " ";
-    }
-    cout << endl;
+    sort(numbers, numbers + n, customComparator);
+    printSequence(numbers, n);
 
     return 0;
 }
 
 int third() {
-    int n, m, k;
+    int n;
 
-    srand(time(nullptr));
+    cout << "Введите размерность матрицы: ";
+    cin >> n;
 
-    cout << "Введите количество n и m (n, m <= 100): ";
-    cin >> n >> m;
-
-    cout << "Введите ограничение для элементов матрицы: ";
-    cin >> k;
-
-    vector<vector<int>> matrix(n, vector<int>(m));
+    int matrix[n][n];
 
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            matrix[i][j] = rand() % (2 * k + 1) - k;
-            // cin >> matrix[i][j];
+        for (int j = 0; j < n; ++j) {
+            cin >> matrix[i][j];
         }
     }
 
-    printMatrix(matrix);
     cout << endl;
 
-    int minElement = numeric_limits<int>::max();
-    int minColumn = -1;
-    
-    for (int j = 0; j < m; ++j) {
-        for (int i = 0; i < n; ++i) {
-            if (matrix[i][j] < minElement) {
-                minElement = matrix[i][j];
-                minColumn = j;
+    for (int i = 0; i < n; ++i) {
+        printSequence(matrix[i], n);
+        cout << endl;
+    }
+
+    int minValue = INT_MAX;
+    int minCol = -1;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (matrix[i][j] < minValue) {
+                minValue = matrix[i][j];
+                minCol = j;
             }
         }
     }
-    
-    if (minColumn != -1) {
-        replaceNegativeElements(matrix, minColumn);
 
-        cout << "Изменённая матрица:\n";
-        printMatrix(matrix);
-    } else {
-        cout << "Матрица не содержит элементов." << endl;
+    for (int i = 0; i < n; ++i) {
+        if (matrix[i][minCol] < 0) {
+            matrix[i][minCol] = 0;
+        }
     }
-    
+
+    cout << "Изменённая матрица:" << endl;
+
+    for (int i = 0; i < n; ++i) {
+        printSequence(matrix[i], n);
+        cout << endl;
+    }
+
     return 0;
 }
 
+// int fourth() {
+//     int n;
+
+//     cout << "Введите количество элементов <= 10000: ";
+//     cin >> n;
+
+//     int arr[20'000];
+
+//     cout << "Введите последовательность натуральных чисел:\n";
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     int index = 0;
+
+//     for (int i = 0; i < n; i++) {
+//         if (!hasDuplicates(arr[i])) {
+//             arr[index++] = arr[i];
+//             if (hasFive(arr[i])) {
+//                 arr[index++] = arr[i];
+//             }
+//         }
+//     }
+
+//     cout << "Полученная последовательность (каждый элемент по одному разу):\n";
+//     for (int i = 0; i < index; i++) {
+//         cout << arr[i] << " ";
+//     }
+
+//     return 0;
+// }
+
 int fourth() {
     int n;
-    cout << "Введите количество элементов (n <= 10000): ";
+    int arr[MAX_N];
+
+    cout << "Введите количество элементов <= 10000: ";
     cin >> n;
 
-    vector<int> sequence(n);
-
-    cout << "Введите последовательность:" << endl;
-    
+    cout << "Введите элементы:\n";
     for (int i = 0; i < n; ++i) {
-        cin >> sequence[i];
+        cin >> arr[i];
     }
 
-    vector<int> result;
-    for (int num : sequence) {
-        if (!duplicateDigits(num)) {
-            result.push_back(num);
+    int index = 0;
 
-            int temp = num;
-            bool flag = false;
-
-            while (temp > 0) {
-                if (temp % 10 == 5) {
-                    flag = true;
-                    break;
-                }
-                temp /= 10;
-            }
-            if (flag) {
-                result.push_back(num);
+    for (int i = 0; i < n; ++i) {
+        if (!hasDuplicates(arr[i])) {
+            arr[index++] = arr[i];
+            if (hasFive(arr[i])) {
+                arr[index++] = arr[i];
             }
         }
     }
+    n = index;
 
-    cout << "Итоговая последовательность:" << endl;
+    cout << "Измененная последовательность:\n";
 
-    for (int num : result) {
-        cout << num << " ";
-    }
-    cout << endl;
+    printSequence(arr, n);
 
     return 0;
 }
